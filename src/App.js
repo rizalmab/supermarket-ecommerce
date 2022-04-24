1f9fb5dbfda822b58558b2d2ac4f61ccdd603ab1
rizalmab <rizalmahadiab@gmail.com>
2022-04-24 18:06:39 +0800

    Added sidebar, main section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/NavBar";
+import SingleCard from "./components/ItemCards/SingleCard";
+
+//! Add in filter sidebar, cards for items (with quantity, add to cart button, price)
 
 function App() {
   return (
     <div className="App">
-      <NavBar></NavBar>
+      <NavBar />
+      <div className="main-container">
+        <div className="sidebar">Sidebar</div>
+        <div className="main-section">
+          <div className="title">
+            <h1>A Supermarket page</h1>
+          </div>
+          <div className="buttons">Buttons section</div>
+          <div className="item-cards">
+            <SingleCard />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,42 @@
 import React from "react";
-import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  solid,
-  regular,
-  brands,
-} from "@fortawesome/fontawesome-svg-core/import.macro";
+  Navbar,
+  Nav,
+  Container,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
+
+let cartItems = 0;
 
 const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        {/* <FontAwesomeIcon icon="fa-solid fa-fork-knife" /> */}
-        <FontAwesomeIcon icon={solid("bowl-food")} />
+        <FontAwesomeIcon icon={solid("bag-shopping")} />
         <Navbar.Brand href="#home">Supermarket-Ecommerce</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
+        <Form className="d-flex">
+          <FormControl
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+        <Nav.Link href="#login">Login</Nav.Link>
+        <FontAwesomeIcon icon={solid("cart-shopping")} />
+        <p>{cartItems}</p>
       </Container>
     </Navbar>
   );
