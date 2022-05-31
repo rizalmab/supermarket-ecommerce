b28d1c4530237c21d9496ea245e3ba800c3573e2
rizalmab <rizalmahadiab@gmail.com>
2022-05-31 17:04:02 +0800

    Managed to fetch data from server and render on page

--- src/features/shop/CardsContainer.js
+++ src/features/shop/CardsContainer.js
@@ -1,38 +1,27 @@
 import React, { useEffect, useState } from "react";
 import SingleCard from "./SingleCard";
 import { createServer } from "miragejs";
-import axios from "axios";
+// import axios from "axios";
 import itemsData from "../../config/ItemsData";
-import { useDispatch } from "react-redux";
-import { fetchItems } from "./shopSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchItems, selectCartItems } from "./shopSlice";
 
 let server = createServer();
 server.get("/api/food", itemsData);
 
-// On page load, dispatch a function that will call for API data
-
 const CardsContainer = () => {
   const [items, setItems] = useState([]);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const fetchedData = await axios.get("/api/food");
-  //     setItems(fetchedData.data);
-  //     console.log({
-  //       message: "successfully fetched data",
-  //       data: fetchedData.data,
-  //     });
-  //     console.log("fetchedData", fetchedData);
-  //   };
-
-  //   fetchData();
-  // }, []);
-
   const dispatch = useDispatch();
+  const cartItems = useSelector(selectCartItems);
 
   useEffect(() => {
     dispatch(fetchItems);
   }, []);
+
+  useEffect(() => {
+    setItems(cartItems);
+  }, [cartItems]);
 
   return (
     <>
